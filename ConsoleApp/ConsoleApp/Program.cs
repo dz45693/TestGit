f051432789c0306d79fb7d20557373b04ccae8a2
dz45693 <dz45693@163.com>
2012-07-09 16:21:52 +0800

    create

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello word");
+            Console.ReadLine();
+        }
+    }
+}
